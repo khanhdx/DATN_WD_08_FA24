a6e1e719123e1547f7eb86d206fb35bd60dc25bc
dduc-nguyenn <dducnguyen0969@gmail.com>
2024-09-25 12:35:59 +0700

    Trang Chủ

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        for ($i=0; $i < 10; $i++) { 
+            Post::create([
+                'image'         => fake()->imageUrl,                    // Đường dẫn ảnh
+                'title'         => fake()->text(25),        // Tiêu đề bài viết
+                'content'       => fake()->text(500),       // Nội dung bài viết
+                'author'        => fake()->name(),                      // Tác giả bài viết
+                'publish_date'  => date('Y/m/d'),               // Ngày đăng
+            ]);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\Admins\UserController;
+use App\Http\Controllers\Client\HomeController;
+use App\Http\Controllers\Client\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/admin', function () {
+    return view('admin.admins');
+});
+
+Route::resource('user', UserController::class);
+Route::resource('posts', PostController::class);
+
+
+Route::get('/', [HomeController::class, 'index']);
+
+// Route để hiển thị danh sách bài viết
+Route::get('/posts', [PostController::class, 'clientIndex'])->name('client.posts.index');
+
+// Route để hiển thị chi tiết từng bài viết
+Route::get('/posts/{id}', [PostController::class, 'clientShow'])->name('client.posts.show');
