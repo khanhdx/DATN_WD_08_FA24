6fedb09505b54db61cccede1884ebe3acddd25f7
Cong Hiep <hiepooo94@gmail.com>
2024-09-26 21:05:01 +0700

    xoa check

--- resources/views/check.blade.php
+++
@@ -1,5 +0,0 @@
-<h1>{{ Auth::user()->name }}</h1>
-<form action="{{ route('logout') }}" method="post" style="display: inline;">
-    @csrf
-    <button type="submit" class="au-btn au-btn--red">Đăng Xuất</button>
-</form>
