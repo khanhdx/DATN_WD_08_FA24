4b03b9ef4822d24b31d9769e6e32417d92f3d380
dduc-nguyenn <dducnguyen0969@gmail.com>
2024-09-26 14:49:37 +0700

    update

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -13,13 +13,15 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i=0; $i < 10; $i++) { 
+        for ($i=0; $i < 10; $i++) {
+            $price = rand(100,999);
+            $sale = $price * 0.6; // Giảm 40%
             Product::create([
                 'category_id'   => rand(1, 10),
                 'name'          => fake()->text(20),
                 'SKU'           => "OB" . rand(10000,99999),
-                'price_regular' => rand(100,999),
-                'price_sale'    => rand(10,99),
+                'price_regular' => $price,
+                'price_sale'    => $sale,
                 'description'   => fake()->text(200),
                 'views'         => rand(1,100),
                 'content'       => fake()->text(500),
