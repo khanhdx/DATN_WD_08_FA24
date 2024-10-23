99e03ef0902433976d04441940292ce84b759c80
hoangtuyen2004 <ductuyen772@gmail.com>
2024-10-24 00:06:56 +0700

    user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
             PostSeeder::class,
             VoucherSeeder::class,
             StatusOrderSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
