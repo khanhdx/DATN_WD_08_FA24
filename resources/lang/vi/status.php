f0c3603f1b37b55affcc46baf286f91ca354aba7
hoangtuyen2004 <ductuyen772@gmail.com>
2024-11-28 17:59:10 +0700

    them giao hang thanh cong + that bai

--- resources/lang/vi/status.php
+++ resources/lang/vi/status.php
@@ -4,6 +4,8 @@ return [
     'pending' => 'Chờ xử lý',
     'processing' => 'Đang xử lý',
     'shipping' => 'Đang giao hàng',
+    'failed'=>'Giao hàng thất bại',
+    'success'=>'Giao hàng thành công',
     'completed' => 'Hoàn Thành',
     'cancel' => 'Hủy đơn',
     'canceled' => 'Đã hủy',
